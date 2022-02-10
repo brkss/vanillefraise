c42dc5b7881feb872941eae233f983892f4ec081
Brahim Berkasse <berkassebrahim@gmail.com>
2022-02-10 16:31:13 +0100

    🪨 UI FIX : Safearea Activity Screen !

--- app/src/screens/Activity/Activity.tsx
+++ app/src/screens/Activity/Activity.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { View, Text, StyleSheet, ScrollView, SafeAreaView } from "react-native";
 import { Slider, Heading, ActivityThumbnail } from "../../components";
 import { activity_categories } from "../../utils/data";
 
 export const Activity: React.FC<any> = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.headingContainer}>
-        <Heading title={"Activity"} />
-      </View>
-      <View style={styles.recipesContainer}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <Slider color={"#D9EFB8"} categories={activity_categories} />
-          <ActivityThumbnail />
-        </ScrollView>
-      </View>
+      <SafeAreaView>
+        <View style={styles.headingContainer}>
+          <Heading title={"Activity"} />
+        </View>
+        <View style={styles.recipesContainer}>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Slider color={"#D9EFB8"} categories={activity_categories} />
+            <ActivityThumbnail />
+          </ScrollView>
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
@@ -26,9 +28,9 @@ const styles = StyleSheet.create({
     paddingBottom: 0,
   },
   headingContainer: {
-    flex: 0.14,
+    //flex: 0.14,
   },
   recipesContainer: {
-    flex: 0.86,
+    //flex: 0.86,
   },
 });
