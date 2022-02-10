58d07706cadb8f9090d22a9c92d7b63b1b5c97f2
Brahim Berkasse <berkassebrahim@gmail.com>
2022-02-10 01:20:11 +0100

    ⏱  Reminder Ui

--- app/src/screens/Reminders/Reminder.tsx
+++ app/src/screens/Reminders/Reminder.tsx
@@ -1,10 +1,29 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 
 export const Reminder: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Reminder</Text>
+      <SafeAreaView>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Lunch,</Text>
+          <Text style={styles.subtitle}>Remaning.</Text>
+        </View>
+        <View style={styles.timeContainer}>
+          <View style={styles.timeUnit}>
+            <Text style={styles.unit}>Hours</Text>
+            <Text style={styles.value}>09</Text>
+          </View>
+          <View style={styles.timeUnit}>
+            <Text style={styles.unit}>Minutes</Text>
+            <Text style={styles.value}>19</Text>
+          </View>
+          <View style={styles.timeUnit}>
+            <Text style={styles.unit}>Seconds</Text>
+            <Text style={styles.value}>00</Text>
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
@@ -12,7 +31,34 @@ export const Reminder: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "#0030FF",
+    padding: 15,
+    paddingBottom: 0,
+  },
+  titleContainer: {},
+  title: {
+    fontSize: 53,
+    fontWeight: "bold",
+    color: "white",
+  },
+  subtitle: {
+    fontSize: 53,
+    color: "white",
+  },
+  timeContainer: {
+    marginTop: 40,
+  },
+  timeUnit: {
+    marginBottom: 20,
+  },
+  unit: {
+    fontSize: 34,
+    fontWeight: "bold",
+    color: "white",
+  },
+  value: {
+    fontSize: 69,
+    fontWeight: "bold",
+    color: "white",
   },
 });
