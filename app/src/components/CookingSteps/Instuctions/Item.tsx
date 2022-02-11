281d35631ae85bff1f0decd91de59aa2683dec42
Brahim Berkasse <berkassebrahim@gmail.com>
2022-02-11 19:17:41 +0100

    🦩 Implementing react reanimated !

--- app/src/components/CookingSteps/Instuctions/Item.tsx
+++ app/src/components/CookingSteps/Instuctions/Item.tsx
@@ -1,12 +1,49 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from "react-native-reanimated";
 
 export const Item: React.FC = () => {
+  const offset = useSharedValue({ x: 0, y: 0 });
+  const start = useSharedValue({ x: 0, y: 0 });
+  const isPressed = useSharedValue(false);
+  const gesture = Gesture.Pan()
+    .onBegin(() => {
+      isPressed.value = true;
+    })
+    .onUpdate((e) => {
+      offset.value = {
+        x: e.translationX + start.value.x,
+        y: e.translationY + start.value.y,
+      };
+    })
+    .onEnd(() => {
+      start.value = {
+        x: offset.value.x,
+        y: offset.value.y,
+      };
+    })
+    .onFinalize(() => {
+      isPressed.value = false;
+    });
+  const style = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: offset.value.x },
+        { translateY: offset.value.y },
+        { scale: withSpring(isPressed ? 0.9 : 1) },
+      ],
+    };
+  });
+
   return (
     <View style={styles.container}>
-      <GestureDetector>
-        <View>
+      <GestureDetector gesture={gesture}>
+        <Animated.View style={style}>
           <Text style={styles.number}>#1</Text>
           <Text style={styles.txt}>
             Spray a skillet with cooking spray and heat over medium-high heat.
@@ -14,7 +51,7 @@ export const Item: React.FC = () => {
             5 to 7 minutes total. Transfer to a slow cooker and pour French
             onion soup on top.
           </Text>
-        </View>
+        </Animated.View>
       </GestureDetector>
     </View>
   );
