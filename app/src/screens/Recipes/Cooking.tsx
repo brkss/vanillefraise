893c74761357e9637cdbd06f8dade60ce96ac6b2
Brahim Berkasse <berkassebrahim@gmail.com>
2022-02-11 22:05:11 +0100

    ✨ Cancel Button when you're in cooking mode !

--- app/src/screens/Recipes/Cooking.tsx
+++ app/src/screens/Recipes/Cooking.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import { Start, IngredientStep, InstructionsStep } from "../../components";
 import { useFonts } from "expo-font";
 
-export const Cooking: React.FC = () => {
+export const Cooking: React.FC<any> = ({ navigation }) => {
   const [step, SetStep] = React.useState("start");
   const [helviticaCondensed] = useFonts({
     "helvitica-condesed": require("../../assets/helvitica-condensed.otf"),
@@ -18,15 +24,25 @@ export const Cooking: React.FC = () => {
   return (
     <View style={styles.container}>
       <SafeAreaView style={{ flex: 1 }}>
-        {
+        <View style={styles.top}>
+          <TouchableOpacity
+            style={styles.cancelBtn}
+            onPress={() => navigation.goBack()}
+          >
+            <Text style={styles.cancelBtnText}>Cancel</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.content}>
           {
-            start: <Start finish={() => changeStep("ingredients")} />,
-            ingredients: (
-              <IngredientStep finish={() => changeStep("instructions")} />
-            ),
-            instructions: <InstructionsStep />,
-          }[step]
-        }
+            {
+              start: <Start finish={() => changeStep("ingredients")} />,
+              ingredients: (
+                <IngredientStep finish={() => changeStep("instructions")} />
+              ),
+              instructions: <InstructionsStep />,
+            }[step]
+          }
+        </View>
       </SafeAreaView>
     </View>
   );
@@ -37,5 +53,23 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     backgroundColor: "#FBF0D3",
+  },
+  content: {
+    flex: 1,
+  },
+  top: {
+    flexDirection: "row-reverse",
+    flexWrap: "nowrap",
+  },
+  cancelBtn: {
+    backgroundColor: "#4A4848",
+    padding: 8,
+    borderRadius: 7,
+    minWidth: 100,
+  },
+  cancelBtnText: {
+    color: "white",
+    textAlign: "center",
+    fontFamily: "helvitica-condesed",
   },
 });
