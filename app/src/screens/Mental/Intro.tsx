9f889d2adbf1499837651d74b65aa60b46ebcef8
Brahim Berkasse <berkassebrahim@gmail.com>
2022-02-24 00:56:15 +0100

    🐞 Add Heading !

--- app/src/screens/Mental/Intro.tsx
+++ app/src/screens/Mental/Intro.tsx
@@ -1,43 +1,49 @@
 import React from "react";
 import { View, Text, StyleSheet, SafeAreaView, Dimensions } from "react-native";
 import { MentalIntroCircle } from "../../components/MentalHealth";
+import { useFonts } from "expo-font";
 
 const { width: w, height: h } = Dimensions.get("window");
 
 const circles = [
   {
     x: 0.3,
-    y: 0.4,
+    y: 0,
     d: 100,
     duration: 900,
   },
   {
     x: 0.7,
-    y: 0.5,
+    y: 0.1,
     d: 70,
     duration: 1000,
   },
   {
     x: 0.4,
-    y: 0.5,
+    y: 0.02,
     d: 56,
     duration: 1200,
   },
   {
     x: 0.1,
-    y: 0.3,
+    y: 0.1,
     d: 40,
     duration: 1100,
   },
 ];
 
 export const MentalHealthIntro: React.FC = () => {
+  const [helviticaCondensed] = useFonts({
+    "helvitica-condesed": require("../../assets/helvitica-condensed.otf"),
+  });
+  if (!helviticaCondensed) return <View style={{ flex: 1 }} />;
+
   return (
     <View style={styles.container}>
       <SafeAreaView style={{ flex: 1 }}>
         <View style={styles.headerContainer}>
           <Text style={styles.header}>
-            Improve{"\n"}Your{"\n"}
+            I{"\n"}9{"\n"}80{"\n"}60.
           </Text>
         </View>
         {circles.map((c, key) => (
@@ -70,8 +76,10 @@ const styles = StyleSheet.create({
   },
   headerContainer: {},
   header: {
-    fontSize: 40,
+    fontSize: 70,
+    lineHeight: 70,
     fontWeight: "bold",
-    color: 'white'
+    color: "white",
+    fontFamily: "helvitica-condesed",
   },
 });
