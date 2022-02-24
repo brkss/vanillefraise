d55120555f9627a36b4a44b6f5cb99833e86c4c2
Brahim Berkasse <berkassebrahim@gmail.com>
2022-02-24 16:05:45 +0100

    🧍 User Info : Graphql types for special condition entity !

--- server/src/entity/UserInfo/SpecialCondition.ts
+++ server/src/entity/UserInfo/SpecialCondition.ts
@@ -1,10 +1,15 @@
 import { Entity, BaseEntity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { ObjectType, Field } from 'type-graphql';
 
+@ObjectType()
 @Entity()
 export class SpecialCondition extends BaseEntity {
+  
+  @Field()
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
+  @Field()
   @Column()
   name: string;
 }
