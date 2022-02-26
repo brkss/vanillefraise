24acf5cc488d6823f406af7dbce800774731cd7f
Brahim Berkasse <berkassebrahim@gmail.com>
2022-02-26 19:34:11 +0100

    🧍 R

--- server/src/resolvers/records/create.resolver.ts
+++ server/src/resolvers/records/create.resolver.ts
@@ -1,23 +1,28 @@
-import { Resolver, Mutation, Arg } from 'type-graphql';
+import { Resolver, Mutation, Arg, UseMiddleware, Ctx } from 'type-graphql';
 import { Record, RecordCategory } from '../../entity/Record';
 import { CreateRecordInput } from '../../utils/inputs/records';
 import { CreateRecordResponse } from '../../utils/responses/records';
+import { isUserAuth } from '../../utils/middlewares/auth.mw'; 
+import {IContext} from '../../utils/types/Context';
+import {User} from '../../entity/User';
 
 @Resolver()
 export class CreateRecordResolver {
 
+  @UseMiddleware(isUserAuth)
   @Mutation(() => CreateRecordResponse)
-  async createRecord(@Arg('data') data : CreateRecordInput) : Promise<CreateRecordResponse>{
+  async createRecord(@Arg('data') data : CreateRecordInput, @Ctx() ctx: IContext) : Promise<CreateRecordResponse>{
 
-    if(!data || !data.date || !data.category || !data.time || !data.value){
+    if(!data || !data.date || !data.category || !data.time || !data.value || !ctx.payload || !ctx.payload.userID){
       return {
         status: false,
         message: "Invalid Data !"
       }
     }
     try {
+      const user = await User.findOne({where: {id: ctx.payload.userID}});
       const category = await RecordCategory.findOne({where : {id: data.category}});
-      if(!category){
+      if(!category || !user){
         return {
           status: false,
           message: "Category not found !"
@@ -25,6 +30,7 @@ export class CreateRecordResolver {
       }
       const record = new Record();
       record.category = category;
+      record.user = user;
       record.value = data.value;
       record.time = data.time;
       record.date = data.date;
