8e6796e2b2423998fc87a62c0936f7b9ff386b77
Brahim Berkasse <berkassebrahim@gmail.com>
2022-03-02 19:11:30 +0100

    🦋 Recipe Nutrition : return response !

--- server/src/utils/nutrition/calls.ts
+++ server/src/utils/nutrition/calls.ts
@@ -5,15 +5,20 @@ interface IInput {
   name: string;
 }
 
-const URL : string = `https://api.edamam.com/api/nutrition-details?app_id=${process.env.EDAM_APP_ID}&app_key=${process.env.EDAM_CLIENT_SECRET}`;
+const URL: string = `https://api.edamam.com/api/nutrition-details?app_id=${process.env.EDAM_APP_ID}&app_key=${process.env.EDAM_CLIENT_SECRET}`;
 
 export const recipeNutrition = async (data: IInput) => {
-  axios.post(URL, {
+  const res = await axios.post(URL, {
     title: data.name,
-    ingr: data.ingr
-  }).then(async res => {
+    ingr: data.ingr,
+  });
+  return res;
+  /*
+  .then(async res => {
     console.log("Recipe nutrition results => ", res.data);
+    
   }).catch(e => {
     console.log("Something went wrong while fetching recipe nutrition : ", e);
   })
+  */
 };
