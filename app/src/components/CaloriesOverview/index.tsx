7f1efb64be6ce33a6593ecb1cd5d93f7fd42558e
Brahim Berkasse <berkassebrahim@gmail.com>
2022-03-09 20:10:32 +0100

    🦄 User's calories fetch check and display !

--- app/src/components/CaloriesOverview/index.tsx
+++ app/src/components/CaloriesOverview/index.tsx
@@ -1,33 +1,42 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { LoadingBar } from './LoadingBar';
-import { useCookedRecipesCountQuery } from '../../generated/graphql';
-import { Loading } from '../General/Loading';
+import { LoadingBar } from "./LoadingBar";
+import { useCookedRecipesCountQuery } from "../../generated/graphql";
+import { Loading } from "../General/Loading";
+import { useUserCaloriesQuery } from "../../generated/graphql";
 
 export const CaloriesOverview: React.FC = () => {
-
+  const { data, loading, error } = useUserCaloriesQuery();
   const _count = useCookedRecipesCountQuery();
 
-  if(_count.loading || _count.error){
-    return <Loading />
+  if (
+    _count.loading ||
+    _count.error ||
+    loading ||
+    error ||
+    !data.userCalories.status
+  ) {
+    return <Loading />;
   }
 
   return (
     <View style={styles.container}>
       <View style={styles.caloriesContainer}>
-        <Text style={styles.takenCalories}>1402</Text>
-        <Text style={styles.needCalories}> / 2400</Text>
+        <Text style={styles.takenCalories}>{data.userCalories.value}</Text>
+        <Text style={styles.needCalories}> / {data.userCalories.target}</Text>
       </View>
       <Text style={styles.unit}>calories</Text>
       <LoadingBar />
-      <Text style={styles.recipeCooked}>You cooked  {_count.data.cookedRecipesCount} recipes.</Text>
+      <Text style={styles.recipeCooked}>
+        You cooked {_count.data.cookedRecipesCount} recipes.
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginBottom: 20
+    marginBottom: 20,
   },
   caloriesContainer: {
     flexDirection: "row",
