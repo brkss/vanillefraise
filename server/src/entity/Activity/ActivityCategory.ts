e6abb5c60bf94d165b7c9c0b2e9617f6f5be3777
Brahim Berkasse <berkassebrahim@gmail.com>
2022-03-18 16:21:48 +0100

    🦋 Activity Calories setup ;entity !

--- server/src/entity/Activity/ActivityCategory.ts
+++ server/src/entity/Activity/ActivityCategory.ts
@@ -1,6 +1,7 @@
 import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 import { ObjectType, Field } from "type-graphql";
 import { Activity } from './Activity';
+import { ActivityCalories } from './ActivityCalories';
 
 @ObjectType()
 @Entity("activity_categories")
@@ -16,6 +17,10 @@ export class ActivityCategory extends BaseEntity {
   @Field({ nullable: true })
   @Column({ nullable: true })
   icon?: string;
+
+  @Field(() => [ActivityCalories])
+  @OneToMany(() => ActivityCalories, calories => calories.category)
+  calories: ActivityCalories[];
 
   @Field(() => [Activity])
   @OneToMany(() => Activity, activities => activities.category)
