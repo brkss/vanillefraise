748ea2d2a33fdc9c0fef4bc7e3113c64a25484b8
Brahim Berkasse <berkassebrahim@gmail.com>
2022-04-02 23:14:09 +0000

    🦋 update app metadata !

--- app/src/utils/config/defaults.ts
+++ app/src/utils/config/defaults.ts
@@ -1,5 +1,8 @@
-export const URI = "http://localhost:4000";
-export const CDN = "http://localhost:4000/images";
+//export const URI = "http://localhost:4000";
+//export const CDN = "http://localhost:4000/images";
+
+export const URI = "https://api.opencc.tech";
+export const CDN = "https://api.opencc.tech/images";
 
 //export const URI = "http://10.1.33.146:4000";
 //export const CDN = "http://10.1.33.146:4000/images";
