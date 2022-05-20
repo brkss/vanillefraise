fb59e9d6b58c9817cf869a3ceb95e7130c471bc1
Brahim Berkasse <32434103+brkss@users.noreply.github.com>
2022-05-20 03:41:25 +0100

    🐝 Admin auth setup middleware !

---
+++ server/dist/utils/middlewares/admin.mw.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isUserAuth = void 0;
+const jsonwebtoken_1 = require("jsonwebtoken");
+const isUserAuth = ({ context }, next) => {
+    const authorization = context.req.headers["authorization"];
+    if (!authorization) {
+        throw new Error("Not authenticated !");
+    }
+    const token = authorization.split(" ")[1];
+    if (!token) {
+        throw new Error("Invalid Token !");
+    }
+    try {
+        const payload = (0, jsonwebtoken_1.verify)(token, process.env.ADMIN_ACCESS_SECRET);
+        context.payload = payload;
+    }
+    catch (e) {
+        console.log("Someyhing went wrong ! : ", e);
+        throw new Error("Invalid Token !");
+    }
+    return next();
+};
+exports.isUserAuth = isUserAuth;
+//# sourceMappingURL=admin.mw.js.map
