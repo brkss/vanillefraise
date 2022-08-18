29bf3d837bcba963fc0f60addb003a9d1be69444
Brahim Berkasse <32434103+brkss@users.noreply.github.com>
2022-08-18 01:49:48 +0100

    🔗 connect to edamam api / update requirements and dockerfiel !

--- server/recipes/nutrition.py
+++ server/recipes/nutrition.py
@@ -1,6 +1,31 @@
-import request
+import noms
+import requests
+import json
 
+app_id = "4ae83249"
+app_key = "4093dcfc796693ca3e7c13619b7295b2"
+URL = "https://api.edamam.com/api/nutrition-details?app_id={}&app_key={}".format(app_id, app_key)
 
 def get_nutrition(ingredients):
-    #
-    return [] 
+    print("ingredients : ", ingredients)
+    data = {
+        "title": "Test",
+        "ingr": ingredients
+    }
+    headers={
+        'Content-type':'application/json', 
+        'Accept':'application/json'
+    }
+    res = requests.post(url=URL, headers=headers, json=data)
+    print("status : ", res.status_code)
+    print("results : ", res.json())
+    return []
+
+ingredients = [
+            "2 eggs, beaten",
+            "2 cloves garlic, minced",
+            "4 ounces feta cheese",
+            "1 (10 ounce) box frozen chopped spinach, thawed and squeezed dry",
+            "2 pounds ground turkey"
+        ]
+get_nutrition(ingredients)
