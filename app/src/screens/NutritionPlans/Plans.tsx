d99eb26ea225d9e82d0e4af7ce9dd777736239a4
Brahim Berkasse <32434103+brkss@users.noreply.github.com>
2022-08-23 02:18:05 +0100

    🍃 set up title for nutrition plans screen !

--- app/src/screens/NutritionPlans/Plans.tsx
+++ app/src/screens/NutritionPlans/Plans.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export const NutritionPlans : React.FC = () => {
-
+export const NutritionPlans: React.FC = () => {
   return (
     <View style={styles.container}>
-      <SafeAreaView>
-        <Text>Nutrition Plans </Text>
-    </SafeAreaView>
+      <SafeAreaView style={{flex: 1}}>
+        <ScrollView style={{ flex: 1 }}>
+          <View style={styles.box}>
+            <Text style={styles.title}>Nutrition Plans </Text>
+            <Text style={styles.subtitle}>
+              Apply nutrtion management plans to go micro on certain situations.
+            </Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </View>
-  )  
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
-})
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 14,
+    color: "#6B6B6B",
+  },
+  box: {
+    padding: 15,
+  },
+});
