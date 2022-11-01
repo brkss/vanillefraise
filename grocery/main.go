a6607da98c0ef6b9399be2373d8bf6fead6fb9e8
Brahim Berkasse <32434103+brkss@users.noreply.github.com>
2022-11-01 01:14:14 +0100

    💿 translate ingredients !

--- grocery/main.go
+++ grocery/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	gt "github.com/bas24/googletranslatefree"
 	"github.com/google/uuid"
 )
 
@@ -14,13 +16,20 @@ type IngredientResponse struct {
 }
 
 type GroceryRaw struct {
-	Text string
-	Id   string
+	Text       string
+	Id         string
+	Translated string
 }
 
 const (
 	URL = "http://localhost:4000/grocery"
 )
+
+func printGroceries(groceries []GroceryRaw) {
+	for _, g := range groceries {
+		fmt.Printf("-> %+v \n", g)
+	}
+}
 
 func main() {
 
@@ -32,11 +41,24 @@ func main() {
 		log.Fatal("error getting groceries !")
 	}
 
+	result, _ := gt.Translate("chiken wings", "en", "fr")
+	fmt.Printf("translte : %s \n", result)
+
 	decoder := json.NewDecoder(res.Body)
 	decoder.Decode(&ingredients)
 	for _, ing := range ingredients {
 		id := uuid.New()
-		groceries = append(groceries, GroceryRaw{Text: ing.Ingredients, Id: id.String()})
-		fmt.Println("ing => ", ing.Ingredients)
+		groceries = append(groceries,
+			GroceryRaw{Text: ing.Ingredients, Translated: "none", Id: id.String()})
 	}
+	printGroceries(groceries)
+	// tranlate groceries to frensh !
+	for i := range groceries {
+		result, _ := gt.Translate(groceries[i].Text, "en", "fr")
+		groceries[i].Translated = result
+		fmt.Printf("translated : %+v\n", groceries[i])
+		time.Sleep(time.Second)
+	}
+	fmt.Println("-------------------------------------")
+	printGroceries(groceries)
 }
--- grocery/translate.py
+++ grocery/translate.py
@@ -3,8 +3,12 @@ from time import sleep
 
 src = ["chicken wings", "olive oil", "black pepper", "garlic", "spinach"]
 translator = Translator()
-#res = translator.translate(src, dest="fr")
+res = translator.translate("Melt the butter in a medium-to-large saucepan over medium heat. Once melted, add the oats and stir to coat. Cook, stirring occasionally, until the oats smell toasty and some are starting to turn lightly golden, about 4 to 6 minutes.", dest="ar")
+print("origin : ", res.origin)
+print("ar : ", res.text)
+"""
 for item in src:
     res = translator.translate(item, dest='fr') 
     print(item, " -> ", res.text)
     sleep(1)
+"""
