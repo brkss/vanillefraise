5dfa28b6876e81b618b5a66d7f2797e67dd0ec4c
Brahim Berkasse <berkassebrahim@gmail.com>
2022-12-06 23:02:41 +0100

    🥄 update clean db quries !

--- server/CLEAN_DB.sql
+++ server/CLEAN_DB.sql
@@ -2,3 +2,4 @@
 
 -- Remove keyword Recipe from recipes title !
 UPDATE recipes SET name = REPLACE(name, 'Recipe', '') WHERE name LIKE '%recipe%';
+UPDATE recipes SET name = REPLACE(name, "Easy ", "") WHERE name LIKE '%easy%';
